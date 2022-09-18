1e63e94393eb45aca070ead0ee70832d8879c678
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-09-18 11:54:31 +0200

    overview edit button and css

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -2,7 +2,10 @@ export default function Overview(props) {
     return (
         <div className="overview--task">
             <span>{props.task}</span>
-            <button className="overview--delete" onClick={props.deleteTask}>X</button>
+            <div className="overview--buttons-container">
+                <button className="overview--edit">EDIT</button>
+                <button className="overview--delete" onClick={props.deleteTask}>X</button>
+            </div>
         </div>
     )
 }
