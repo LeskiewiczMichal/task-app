794d900a08d79f5a81c7945a769736d57f25a36a
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-09-18 13:20:36 +0200

    Edit functionality

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Overview from './components/Overview';
 import { nanoid } from 'nanoid';
 
 function App() {
-  const [formData, setFormData] = useState({ taskName: '' });
+  const [formData, setFormData] = useState({ taskName: ' ', edit: ' ' });
   const [tasks, setTasks] = useState([]);
 
   function handleChange(event) {
@@ -17,37 +17,61 @@ function App() {
   }
 
   function newTask() {
-    setTasks(prevTasks => {
-      return [...prevTasks, {text: formData.taskName, id: nanoid()}]
-    })
-    setFormData({taskName: ''})
+    setTasks((prevTasks) => {
+      return [...prevTasks, { text: formData.taskName, id: nanoid() }];
+    });
+    setFormData({ taskName: '' });
   }
-
 
   function deleteTask(taskId) {
     setTasks((prevTasks) => {
-      return prevTasks.filter((task) => task.id !== taskId)
-    })
+      return prevTasks.filter((task) => task.id !== taskId);
+    });
+  }
+
+  function confirmEdit(taskId) {
+    setTasks((prevTasks) => {
+      const newTasks = [];
+      for (let i = 0; i < prevTasks.length; i++) {
+        const task = prevTasks[i];
+        if (task.id === taskId) {
+          newTasks.push({ ...task, text: formData.edit });
+        } else {
+          newTasks.push(task);
+        }
+      }
+      return newTasks;
+    });
   }
 
   const renderTasks = tasks.map((task) => {
-    return <Overview task={task.text} key={task.id} id={task.id} deleteTask={() => deleteTask(task.id)}/>
-  })
+    return (
+      <Overview
+        task={task.text}
+        key={task.id}
+        id={task.id}
+        deleteTask={() => deleteTask(task.id)}
+        confirmEdit={() => confirmEdit(task.id)}
+        formData={formData}
+        handleChange={handleChange}
+      />
+    );
+  });
 
   return (
     <div className="App">
-      <div className='app--tasks-container'>
-        {renderTasks}
-      </div>
+      <div className="app--tasks-container">{renderTasks}</div>
       <input
         type="text"
         placeholder="Task..."
         onChange={handleChange}
         name="taskName"
         value={formData.taskName}
-        className='app--input'
+        className="app--input"
       />
-      <button className='app--button' onClick={newTask}>Add task</button>
+      <button className="app--button" onClick={newTask}>
+        Add task
+      </button>
     </div>
   );
 }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,11 +1,44 @@
+import { useState } from 'react';
+
 export default function Overview(props) {
-    return (
-        <div className="overview--task">
-            <span>{props.task}</span>
-            <div className="overview--buttons-container">
-                <button className="overview--edit">EDIT</button>
-                <button className="overview--delete" onClick={props.deleteTask}>X</button>
-            </div>
-        </div>
-    )
+  const [editingTask, setEditngTask] = useState(false);
+  function editTask(taskId) {
+    setEditngTask((prevEditingTask) => !prevEditingTask);
+  }
+
+  return (
+    <div className="overview--task">
+      <span>{props.task}</span>
+      <div className="overview--buttons-container">
+        {editingTask && (
+          <input
+            type="text"
+            name="edit"
+            onChange={props.handleChange}
+            value={props.formData.edit}
+            placeholder="New value..."
+            className="app--edit-input"
+          />
+        )}
+        {!editingTask ? (
+          <button className="overview--edit" onClick={editTask}>
+            EDIT
+          </button>
+        ) : (
+          <button
+            className="overview--edit"
+            onClick={() => {
+              props.confirmEdit();
+              editTask();
+            }}
+          >
+            ACCEPT
+          </button>
+        )}
+        <button className="overview--delete" onClick={props.deleteTask}>
+          X
+        </button>
+      </div>
+    </div>
+  );
 }
